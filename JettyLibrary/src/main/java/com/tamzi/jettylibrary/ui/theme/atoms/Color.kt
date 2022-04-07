00cc5cb1e614630b3643b3d7512df5e13d05d03b
tamzi <tamrefrank@gmail.com>
2022-04-07 17:29:40 +0300

    Initial commit

---
+++ JettyLibrary/src/main/java/com/tamzi/jettylibrary/ui/theme/atoms/Color.kt
@@ -0,0 +1,33 @@
+package com.tamzi.kahawabucks.ui.theme.atoms
+
+import android.graphics.Color
+
+
+val white = Color(0xFFFCFCFC)
+
+
+val black = Color(0xFF262626)
+val grey = Color(0xFF9D9D9D)
+val lightGrey = Color(0xFFEFEFEF)
+
+
+
+val kahawaBeige = Color(0xFFFBA24C)
+
+val kahawaBrown = Color(0xFFB78358)
+val kahawaDeepBrown = Color(0xFF541F08)
+val kahawaLightBrown = Color(0xFFAF620E)
+val kahawaSmoke = Color(0xFFD6B59C)
+val kahawaCream = Color(0xFFDDAA78)
+val kahawaGrey = Color(0xFF9D9D9D)
+val kahawaDarkGrey = Color(0xFF353439)
+val kahawaBlack = Color(0xFF1B1115)
+
+
+val kahawaBoldGreen = Color(0xFF33895F)
+val kahawaLightGreen = Color(0xFF00924F)
+val kahawaPink = Color(0xFFFF9E9E)
+
+
+
+
